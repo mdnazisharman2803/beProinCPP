c8fdb1282d760407cde4b2b6b86c321b0ca96b2d
mdnazisharman2803 <md.arman.e21@nsut.ac.in>
2022-04-01 19:49:09 +0530

    characterarrays

---
+++ characterarrays/largestwordinstring.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+    int main(){
+
+    int n;
+    cin>>n;
+    cin.ignore();
+    char arr[n+1];
+    cin>>arr;
+   
+    cin.getline(arr,n);
+    cin.ignore();
+    int i=0;
+    int currlen=0;
+    int maxlen =0;
+    int st=0,maxst=0;
+
+    while(1){
+        if(arr[i]==' '||arr[i]=='\0'){
+            
+            if(currlen>maxlen){
+                maxlen=currlen;
+                maxst=st;
+            }
+            currlen=0;
+            st=i+1;
+        }
+        else{
+             currlen++;
+        }
+        if(arr[i]=='\0'){
+             break;
+             }
+            
+        i++;
+    }
+
+   cout<<maxlen<<endl;
+   for(int i=0;i<maxlen;i++){
+       cout<<arr[i+maxst];
+   }
+}

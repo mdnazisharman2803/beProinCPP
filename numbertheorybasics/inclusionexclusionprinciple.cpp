4c2b9dd374a26185866504871c8276b792ebc57c
mdnazisharman2803 <md.arman.e21@nsut.ac.in>
2022-04-06 17:51:54 +0530

    numbertheorybasics

---
+++ numbertheorybasics/inclusionexclusionprinciple.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+// no. of ways to do something
+int divisible(int n, int a, int b){
+    int c1=n/a;
+    int c2=n/b;
+    int c3=n/(a*b);
+    return c1+c2-c3;
+}
+int main(){
+    int n,a,b;
+    cin>>n>>a>>b;
+    cout<<divisible(n,a,b);
+}
